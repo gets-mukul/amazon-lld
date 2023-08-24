18fb7bb2860f448f690a03353c8e0455f347ab81
Mukul <mukulpraj9@gmail.com>
2023-08-24 13:32:30 +0530

    adding readme file

---
+++ src/main/java/com/amazonlld/AmazonLldApplication.java
@@ -0,0 +1,13 @@
+package com.amazonlld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonLldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonLldApplication.class, args);
+	}
+
+}
